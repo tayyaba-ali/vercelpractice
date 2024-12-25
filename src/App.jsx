96ede7d3e17509cca8cd26c896d77f414295bcc6
wasifa-khan <wasifaasadk@gmail.com>
2024-12-25 10:44:39 +0500

    light-dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { GoSun } from "react-icons/go";
+import { FaMoon } from "react-icons/fa";
 
 function App() {
 
+ let [isLit,setLit] = useState(true)
+
+  function changeTheme() {
+      setLit(!isLit)
+      console.log(isLit)
+  }
 
   return (
-    <>
-      <h1>hello vercel</h1>
-    </>
+    <div className={isLit?"light":"dark"}>
+      <label>
+        <input onClick={changeTheme} className="hidden" type="checkbox" />
+        {isLit?<GoSun />:<FaMoon />}
+      </label>
+    </div>
   );
 }
 
